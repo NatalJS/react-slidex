5128b62489493d9da32d64fdb15bd0e5723bfe99
Leonardo Nascimento <leo386386@gmail.com>
2018-10-02 11:51:49 -0300

    Improving the code style

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./containers/HomePage/HomePage.js";
 
-const Index = () => {
-	return <HomePage />;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
-export default Index;
+ReactDOM.render(<HomePage />, document.getElementById("index"));
